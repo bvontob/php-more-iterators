b39b9a51ab7361574763f8130e08132745ffa21f
Beat Vontobel <bvontob@users.noreply.github.com>
2015-05-20 09:17:39 +0000

    Finalize SplitIterator code
    Think through more of the edge cases on the SplitIterator, fix them, and
    decide on what we really need to have "final" (nothing) and/or "private"
    (the splitOff()).

--- SplitIterator.php
+++ SplitIterator.php
@@ -15,38 +15,43 @@ abstract class SplitIterator extends IteratorIterator {
 
   private $key;
 
-  final public function key() {
+  public function __construct(Traversable $innerIterator) {
+    parent::__construct($innerIterator);
+    $this->splitOff();
+  }
+
+  public function key() {
     return $this->key;
   }
 
-  final public function current() {
+  public function current() {
     return $this->splitIterator;
   }
 
-  final public function next() {
+  public function next() {
     parent::next();
     $this->key++;
     $this->splitOff();
   }
 
-  final public function rewind() {
+  public function rewind() {
     parent::rewind();
     $this->key = 0;
     $this->splitOff();
   }
 
-  protected function splitOff() {
-    if(isset($this->splitIterator))
+  private function splitOff() {
+    if(isset($this->splitIterator)) {
       $this->splitIterator->invalidate();
-    $this->splitIterator = new SplitInnerIterator($this->getInnerIterator(), $this);
+      $this->splitIterator = NULL;
+    }
+
+    if(parent::valid())
+      $this->splitIterator = new SplitInnerIterator($this->getInnerIterator(), $this);
   }
 
-  /**
-   * @todo We need to anticipate here, if we really can return a
-   *     SplitInnerIterator, and not just stupidly forward.
-   */
-  final public function valid() {
-    return parent::valid();
+  public function valid() {
+    return isset($this->splitIterator);
   }
 }
 ?>
